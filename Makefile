a64e982847669b2f03f68d255984bf3659b4fdd5
uicus <jakubsutowicz@gmail.com>
2018-01-10 10:49:40 +0100

    Setup repo

---
+++ Makefile
@@ -0,0 +1,22 @@
+CXX = g++
+CXXFLAGS = -Wall -Wextra -Wpedantic -std=c++11 -O3 -flto -s
+CXXFLAGSDEBUG = -Wall -Wextra -Wpedantic -std=c++11 -g
+EXEC = rbg2gdl
+INC = -IrbgParser/src
+OBJECTS =
+
+all: $(OBJECTS)
+	cd rbgParser && make lib
+	$(CXX) $(CXXFLAGS) $(INC) -o $(EXEC) src/main.cpp rbgParser/rbgParser.so $(OBJECTS)
+
+debug: $(OBJECTS)
+	cd rbgParser && make lib
+	$(CXX) $(CXXFLAGSDEBUG) $(INC) -o $(EXEC) src/main.cpp rbgParser/rbgParser.so $(OBJECTS)
+
+clean:
+	rm -f $(OBJECTS)
+	cd rbgParser && make clean
+
+distclean: clean
+	rm -f $(EXEC)
+	cd rbgParser && make distclean
---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+
+#include"options.hpp"
+#include"token.hpp"
+#include"message.hpp"
+#include"game_items.hpp"
+#include"macro_bank.hpp"
+#include"slice_iterator.hpp"
+
+int main(int argc, const char** argv){
+    if(argc < 2){
+        std::cerr<<"No input specified"<<std::endl;
+        std::cerr<<"Usage: \"rbg2gdl [flags] input_file\""<<std::endl;
+        std::cerr<<"Available flags:"<<std::endl;
+        std::cerr<<"\"-o output_file\" - write output to file with given name; defaults to \"a.gdl\""<<std::endl;
+        std::cerr<<"\"-p\" - just preprocess"<<std::endl;
+        std::cerr<<"\"-v\" - just verify input; do not generate output file"<<std::endl;
+        std::cerr<<"\"-s\" - translate to simple form"<<std::endl;
+        std::cerr<<"\"-g\" - translate to gdl (default)"<<std::endl;
+        std::cerr<<"\"-Whide\" - do not show warnings"<<std::endl;
+        std::cerr<<"\"-Werror\" - treat warnings as errors"<<std::endl;
+        std::cerr<<"\"-unfold\" - unfold moves in simple form if they are under ^n"<<std::endl;
+        std::cerr<<"\"-skip-comments\" - do not generate comments in *.gdl"<<std::endl;
+        std::cerr<<"\"-skip-base\" - do not generate base predicates in *.gdl"<<std::endl;
+        std::cerr<<"\"-skip-input\" - do not generate input predicates in *.gdl"<<std::endl;
+    }
+    else{
+        rbg_parser::messages_container msg;
+        try{
+            std::string input_file_name(argv[argc-1]);
+            rbg_parser::options o(argc-2, argv+1);
+            std::ifstream t(input_file_name);
+            std::stringstream buffer;
+            buffer << t.rdbuf();
+            std::ofstream out(o.output_file());
+            if(!out.good())
+                throw msg.build_message("Couldn't open file "+o.output_file());
+            std::vector<rbg_parser::token> result = rbg_parser::tokenize(buffer.str(),msg);
+            rbg_parser::game_items g = rbg_parser::input_tokens(result,msg);
+            rbg_parser::parsed_game pg = g.parse_game(msg);
+            std::cerr<<"Straightness = "<<pg.get_straightness()<<std::endl;
+            if(o.verifying()){
+                if(msg.is_empty())
+                    std::cout<<"Verification successful"<<std::endl;
+            }
+            else if(o.just_preprocess()){
+                out<<pg.to_rbg(true);
+            }
+            else{
+                // print GDL
+            }
+        }
+        catch(rbg_parser::message& m){
+            std::cout<<m.as_error()<<std::endl;
+        }
+        catch(std::exception& e){
+            std::cout<<e.what()<<std::endl;
+        }
+        msg.write_as_warnings(std::cout);
+    }
+    return 0;
+}
